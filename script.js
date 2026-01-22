c865e9fafe888fef11819e2ae591993cdc673077
yngGooby <diegoperez.a323@gmail.com>
2026-01-22 17:55:46 -0500

    Finished the “Odin project Rock, Paper, Scissor” project. Had issues that were resolved with time.
    Issues with prompt appearing an extra time
    
    Was fixed by removing Odin’s suggested variable that was linked to the function
    
    let humanSelection = humanChoice();
    → made it get called an extra time
    
    Need to be persistent/constant with variable name; found some variable names used made the program randomly not work.
    
    Added a while loop to determine when the player or computer has won. One needs to achieve 3 wins.
    
    The else statements were causing harm; they were always printed.
    
    Fixed by using else if() statements.

--- script.js
+++ script.js
@@ -15,43 +15,55 @@
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanSelection, computerSelection){
+    if(humanSelection === computerSelection){
+        console.log("uh oh! same choice");
+    }
+    else if(humanSelection === "rock" && computerSelection === "scissor"){
+        console.log("Rock beats scissors! +1 human score!");
+        humanScore++;
+    }
+    else if(computerSelection === "paper" && humanSelection === "rock"){
+        console.log("Paper beats rock... +1 computer score");
+        computerScore++;
+    }
+    else if(humanSelection === "scissor" && computerSelection === "paper"){
+        console.log("Scissors beat paper! +1 human score!");
+        humanScore++;
+    }
+    else if(computerSelection === "rock" && humanSelection === "scissor"){
+        console.log("Rock beats scissors... +1 computer score");
+        computerScore++;
+    }
+    else if(humanSelection === "paper" && computerSelection === "rock"){
+        console.log("Paper beats rock! +1 human score!")
+        humanScore++;
+    }
+    else if(humanSelection === "paper" && computerSelection === "scissor"){
+        console.log("Scissor beats paper... +1 computer score");
+        computerScore++;
+    }
+};
+
+while(humanScore != 3 && computerScore != 3){
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log("human score: " + humanScore + " Computer score: " + computerScore);
+    if(humanScore == 3){
+        console.log("congragulations! You win!");
+        break;
+    }
+    else if(computerScore == 3){
+        console.log("Sorry, you lose!");
+        break;
+    }
+}
 
 function getComputerChoice(){
     const choices = ["rock", "paper", "scissor"];
-    let ranomValue = choices[Math.floor(choices.length * Math.random())];
-    return ranomValue;
+    let randomValue = choices[Math.floor(choices.length * Math.random())];
+    return randomValue;
 }
 function getHumanChoice(){
     let choice = prompt("make a choice");
     return choice.toLowerCase();
 }
-
-function playRound(humanSelection, getComputerChoice){
-    if(humanSelection === computerSelection){
-        console.log("uh oh! same choice")
-    }
-    if(humanSelection === "rock" && getComputerChoice === "scissor"){
-        console.log("Rock beats scissors! +1 human score!");
-        return humanScore++;
-    }
-    else if(getComputerChoice === "paper" && humanSelection === "rock"){
-        console.log("Paper beats rock... +1 computer score");
-    }
-    if(humanSelection === "scissor" && getComputerChoice === "paper"){
-        console.log("Scissors beat paper! +1 human score!");
-        return humanScore++
-    }
-    else if(computerSelection === "rock" && humanSelection === "scissor"){
-        console.log("Rock beats scissors... +1 computer score");
-    }
-    if(humanSelection === "paper" && getComputerChoice === "rock"){
-        console.log("Paper beats rock! +1 human score!")
-    }
-
-    
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
